05b73569b776a8c5e40e55d008dc3e05e49ebafa
mooud.hosseini <mooudh@gmail.com>
2024-03-30 21:52:36 +0330

    First React Practises

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Another from "./Another";
 
 function App() {
+  const style = {
+    backgroundColor: "blue",
+    fontSize: "25px",
+  };
+  const [count, setCount] = useState(0);
+
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+  const increment = () => {
+    setCount((prev) => prev + 5);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <Another />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {4 > 6 ? (
+          <p style={style}>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        ) : (
+          <div>
+            <p>{count}</p>
+            <div>
+              <button onClick={decrement}>-</button>
+            </div>
+            <div>
+              <button onClick={increment}>+</button>
+            </div>
+          </div>
+        )}
       </header>
     </div>
   );
